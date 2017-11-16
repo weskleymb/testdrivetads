48324d137c73a9544f8c695f74a7ab6bdf262f80
Weskley Bezerra <weskleymb@hotmail.com>
2017-11-16 16:57:35 -0300

    Commit Inicial

---
+++ TestDrive2/TestDrive2/Views/Agendamento.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive2.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive2.Views
+{
+    public partial class Agendamento : ContentPage
+    {
+        public Veiculo Veiculo { get; set; }
+        public Agendamento(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+            BindingContext = this;
+        }
+    }
+}
--- TestDrive2/TestDrive2/Views/Detalhe.xaml.cs
+++ TestDrive2/TestDrive2/Views/Detalhe.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TestDrive2.Models;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace TestDrive2.Views
 {
@@ -14,12 +9,54 @@ namespace TestDrive2.Views
 
         public Veiculo Veiculo { get; set; }
 
+        private Boolean temArCondicionado;
+        public Boolean TemArCondicionado
+        {
+            get
+            {
+                return temArCondicionado;
+            }
+            set
+            {
+                temArCondicionado = value;
+                DisplayAlert("Teste", "Mudou: " + temArCondicionado, "OK");
+                Veiculo.Preco += 1500;
+                OnPropertyChanged(nameof(temArCondicionado));
+            }
+        }
+
+        private string valorFinal;
+        public string ValorFinal
+        {
+            get
+            {
+                return valorFinal;
+            }
+            set
+            {
+                valorFinal = value;
+                if (temArCondicionado)
+                {
+                    valorFinal = string.Format("Preço total: {0}", Veiculo.Preco + 1500);
+                } else
+                {
+                    valorFinal = string.Format("Preço total: {0}", Veiculo.Preco - 1500);
+                }
+            }
+        }
+
         public Detalhe(Veiculo veiculo)
         {
             InitializeComponent();
             this.Veiculo = veiculo;
 
             this.BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            Navigation.PushAsync(new Agendamento(Veiculo));
         }
     }
 }
